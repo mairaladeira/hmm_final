276ae67aaa42c6c92b42312ec84d5218fbf64d31
Maira Machado Ladeira <mairamachadoladeira@gmail.com>
2016-03-06 02:41:00 +0100

    bug fix for viterbi

--- algorithms/viterbi_algorithm.py
+++ algorithms/viterbi_algorithm.py
@@ -10,37 +10,44 @@ class ViterbiAlgorithm(ForwardBackwardAlgorithm):
         ForwardBackwardAlgorithm.__int__(self, a, b, states, pi, finals, obs)
 
     def viterbi(self):
-        viterbi = []
-        i = 0
-        prev_viterbi = {}
+        deltas = []
+        psis = []
         self.path = []
-        most_likely = ('', 0)
-        for obs in self.obs:
-            cur_viterbi = {}
-            total_prob = 0.0
-            for s in self.states:
-                if i == 0:
-                    cur_viterbi[s] = self.pi[s] * \
-                                     self.get_emission_prob(s, obs)
-                    total_prob += cur_viterbi[s]
+        for t in range(len(self.obs)):
+            delta = {}
+            psi = {}
+            for i in self.states:
+                if t == 0:
+                    delta[i] = self.pi[i]*self.get_emission_prob(i, self.obs[t])
+                    psi[i] = 0
                 else:
-                    max_prob = 0
-                    for s2 in self.states:
-                        new_prob = prev_viterbi[s2]*self.get_transition_prob(s2, s)
-                        if new_prob > max_prob:
-                            max_prob = new_prob
-                    cur_viterbi[s] = max_prob * self.get_emission_prob(s, obs)
-                    total_prob += cur_viterbi[s]
-            prev_viterbi = cur_viterbi
-            viterbi.append(cur_viterbi)
-            most_likely = ('', 0)
-            for s in cur_viterbi:
-                if cur_viterbi[s] > most_likely[1]:
-                    most_likely = (s, cur_viterbi[s])
-
-            self.path.append(str(obs)+" "+str(most_likely[0]))
-            i += 1
-        self.prob_path = most_likely[1]/self.prob_obs
+                    max_delta = 0
+                    max_psi = None
+                    for j in self.states:
+                        p = deltas[t-1][j]*self.get_transition_prob(j, i)*self.get_emission_prob(i, self.obs[t])
+                        if p > max_delta:
+                            max_delta = p
+                            max_psi = j
+                    delta[i] = max_delta
+                    psi[i] = max_psi
+            deltas.append(delta)
+            psis.append(psi)
+        max_prob = 0
+        max_state = None
+        for s in deltas[len(deltas)-1]:
+            if deltas[len(deltas)-1][s] > max_prob:
+                max_prob = deltas[len(deltas)-1][s]
+                max_state = s
+        for t in range(len(self.obs)-1):
+            mp = 0
+            ms = None
+            for s in deltas[t+1]:
+                if deltas[t+1][s] > mp:
+                    mp = deltas[t+1][s]
+                    ms = s
+            self.path.append(str(self.obs[t])+": "+str(psis[t+1][ms]))
+        self.path.append(str(self.obs[len(self.obs)-1])+": "+str(max_state))
+        self.prob_path = max_prob/self.prob_obs
 
     def __getitem__(self, item):
         if item == 'prob_path':
